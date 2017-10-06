22d5202221396f1c3c2847630aff2f533459e3df
toregard <dragerot@gmail.com>
2017-10-06 10:27:35 +0200

    merge

--- src/main/java/net/toregard/jerseydemo/JerseydemoApplication.java
+++ src/main/java/net/toregard/jerseydemo/JerseydemoApplication.java
@@ -1,35 +1,15 @@
 package net.toregard.jerseydemo;
-
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
-@SpringBootApplication(exclude = {EmbeddedServletContainerAutoConfiguration.class,
-        WebMvcAutoConfiguration.class})
-public class JerseydemoApplication extends SpringBootServletInitializer {
-
-    public static void main(String[] args) {
-        SpringApplication.run(applicationClass, args);
-    }
-
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(applicationClass);
-//    }
-
-    private static Class<JerseydemoApplication> applicationClass = JerseydemoApplication.class;
-}
-
-/*
+@SpringBootApplication
 public class JerseydemoApplication extends RepositoryRestConfigurerAdapter {
 
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(JerseydemoApplication.class).
-				initializers(new SpringApplicationContextInitializer())
-				.application()
-				.run(args);
-	}
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(JerseydemoApplication.class).
+                initializers(new SpringApplicationContextInitializer())
+                .application()
+                .run(args);
+    }
 }
- */
