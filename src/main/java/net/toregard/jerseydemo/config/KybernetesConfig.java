b2a471abbf29085dc9611e40cffe2272d4a0b021
toregard <dragerot@gmail.com>
2017-10-12 23:22:16 +0200

    ryddet opp

--- src/main/java/net/toregard/jerseydemo/config/KybernetesConfig.java
+++
@@ -1,4 +0,0 @@
-package net.toregard.jerseydemo.config;
-
-public class KybernetesConfig {
-}
--- src/main/java/net/toregard/jerseydemo/restresources/AdminResource.java
+++ src/main/java/net/toregard/jerseydemo/restresources/AdminResource.java
@@ -1,4 +1,21 @@
 package net.toregard.jerseydemo.restresources;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement(name = "admin")
+@Path("/admin")
 public class AdminResource {
+    @GET
+    @Produces("application/json")
+    public String getImage() {
+
+        return "hallo";
+    }
 }
